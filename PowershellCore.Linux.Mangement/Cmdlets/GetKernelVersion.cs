3b44460954b95d988a4b13bc97afa77413647ba4
Daniel Haanpaa <dhaanpaa@djhent.us>
2018-04-01 20:12:09 -0700

    Added Kernel Version Cmdlet

---
+++ PowershellCore.Linux.Mangement/Cmdlets/GetKernelVersion.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Management.Automation;
+
+namespace PowershellCore.Linux.Mangement.Cmdlets
+{
+    [Cmdlet(VerbsCommon.Get, "KernelVersion")]
+    public class GetKernelVersion : PSCmdlet
+    {
+        private CommandLineInterface _commandLineInterface;
+
+        public GetKernelVersion()
+        {
+            _commandLineInterface = new CommandLineInterface();
+        }
+
+        protected override void EndProcessing()
+        {
+            var runCommand = _commandLineInterface.RunCommand("uname -a").SplitStringBySpaces();
+            WriteObject(new
+            {
+                OS = runCommand[0],
+                HostName = runCommand[1],
+                KernelVersion = runCommand[2],
+                Architecture = runCommand[runCommand.Length-1]
+            });
+        }
+    }
+}
---
+++ PowershellCore.Linux.Mangement/CommandLineInterface.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+namespace PowershellCore.Linux.Mangement
+{
+    public class CommandLineInterface
+    {
+        public string RunCommand(string str)
+        {
+            var escapedArgs = str.Replace("\"", "\\\"");
+            
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"{escapedArgs}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return result;
+        }
+
+        
+    }
+
+    public static class StringExts
+    {
+        public static string[] SplitStringBySpaces(this string str)
+        {
+            return str.Split(' ');
+        }
+    }
+}
